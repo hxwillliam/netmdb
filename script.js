6700f9d4996ad548ae1bc4504181464e995098ef
hxwilliam <hxwilliam@skiff.com>
2024-03-11 02:39:33 +0100

    finally works

--- script.js
+++ script.js
@@ -1,48 +1,9 @@
+
+
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = 'api_key=6408e0e2d7398dc7d8998cb43aed056e';
-const API_URL = BASE_URL +'/movie/11?'+ API_KEY;
+const API_URL = BASE_URL +'/movie/popular?'+ API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
-
-/*document.addEventListener('DOMContentLoaded', () => {
-  const main = document.getElementById('main');
-
-  const getMovies = (url) => {
-    fetch(url)
-      .then(res => res.json())
-      .then(data => {
-        console.log(data.results);
-        showMovies(data.results);
-      });
-  };
-
-  const showMovies = (data) => {
-    main.innerHTML = '';
-    data.forEach(movie => {
-      const { title, poster_path, vote_average, overview } = movie;
-      const movieEl = document.createElement('div');
-      movieEl.classList.add('movie');
-      movieEl.innerHTML = `
-        <img src="${IMG_URL + poster_path}" alt="${title}">
-        <div class="movie-info">
-          <span>${title}</span>
-          <span="${IMG_URL + poster_path}">${vote_average}</span>
-        </div>
-        <br></br>
-        <article class="overview">
-          ${overview}
-        </article>
-      `;
-
-      main.appendChild(movieEl);
-    });
-  };
-*/
-  const getColor = (vote) => {
-    return vote >= 8 ? 'green' : vote >= 5 ? 'orange' : 'red';
-  };
-
-  getMovies(API_URL);
-});
 
 document.addEventListener('DOMContentLoaded', () => {
   const main = document.getElementById('main');
@@ -51,7 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
     fetch(url)
       .then(res => res.json())
       .then(data => {
-        console.log(data.results);
+        console.log(data);
         showMovies(data.results);
       });
   };
@@ -63,19 +24,28 @@ document.addEventListener('DOMContentLoaded', () => {
       const movieEl = document.createElement('div');
       movieEl.classList.add('movie');
       movieEl.innerHTML = `
-        <img src="${IMG_URL + poster_path}" alt="${title}">
-        <div class="movie-info">
+
+
+      <img src=${IMG_URL + poster_path}" alt="${title}">
+
+      <div class="movie-info">
           <span>${title}</span>
-          <span class="${getColor(vote_average)}">${vote_average}</span>
-        </div>
-        <br></br>
-        <article class="overview">
-          ${overview}
-        </article>
+          <span> (punteggio:${vote_average})</span>
+      </div>
+      <br>
+      <article class="overview">
+      ${overview}
+      </article>
       `;
+
       main.appendChild(movieEl);
     });
   };
 
+  const getColor = (vote) => {
+    return vote >= 8 ? 'green' : vote >= 5 ? 'orange' : 'red';
+  };
+
   getMovies(API_URL);
 });
+
