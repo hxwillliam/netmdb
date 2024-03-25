48d6c1a98d6eaf9de6f1e4da5605810a776761c6
hxwilliam <hxwilliam@skiff.com>
2024-03-25 10:59:33 +0100

    update

--- script.js
+++ script.js
@@ -35,10 +35,14 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   };
   /*const form = document.getElementById('form');
+  const search = document.getElementById('search');
   form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const searchTerm = search.value;
+
+    console.log('Form submitted'); 
+    console.log('Search term:', searchTerm);
 
     if (searchTerm) {
       getMovies(SEARCH_API_URL + '&query=' + searchTerm);
