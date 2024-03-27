e8e030151a8015a404bee24227d9909bdc20e51c
hxwilliam <hxwilliam@skiff.com>
2024-03-27 14:44:04 +0100

    api

---
+++ api.js
@@ -0,0 +1,4 @@
+const API_KEY = 'api_key=6408e0e2d7398dc7d8998cb43aed056e';
+const BASE_URL = "https://api.themoviedb.org/3";
+const API_URL = BASE_URL + "/movie/popular?" + API_KEY;
+const IMG_URL = "https://image.tmdb.org/t/p/w500";
