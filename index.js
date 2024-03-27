692bd064a275167a7514ae82a2bf0b9720218724
hxwilliam <hxwilliam@skiff.com>
2024-03-27 15:31:39 +0100

    update

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-import { API_URL, IMG_URL, BASE URL, API KEY } from "./api.js";
+import { API_URL, IMG_URL, BASE_URL, API_KEY } from "./api.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   const main = document.getElementById("main");
-
 
   const getMovies = async (url) => {
     try {
@@ -11,14 +10,14 @@ document.addEventListener("DOMContentLoaded", () => {
       console.log(data);
       showMovies(data.results);
     } catch (error) {
-      console.log('Error:', error);
+      console.log("Error:", error);
       setTimeout(() => {
         getMovies(url);
       }, 2000);
     }
   };
   const showMovies = (data) => {
-    main.innerHTML = ""
+    main.innerHTML = "";
 
     data.forEach((movie) => {
       const {
@@ -31,37 +30,34 @@ document.addEventListener("DOMContentLoaded", () => {
         vote_average = movie,
       } = movie;
 
-
-      const movieEl = document.createElement('div');
-      const imgEl = document.createElement('img');
-      const infoEl = document.createElement('div');
-      const titleEl = document.createElement('span');
-      const voteEl = document.createElement('span');
+      const movieEl = document.createElement("div");
+      const imgEl = document.createElement("img");
+      const infoEl = document.createElement("div");
+      const titleEl = document.createElement("span");
+      const voteEl = document.createElement("span");
 
       const getColor = (vote) => {
         if (vote >= 7) {
-          return 'green';
+          return "green";
         } else if (vote >= 5) {
-          return 'orange';
+          return "orange";
         } else {
-          return 'red';
+          return "red";
         }
       };
-      movieEl.classList.add('movie');
+      movieEl.classList.add("movie");
       imgEl.src = IMG_URL + poster_path;
       imgEl.alt = title;
-      infoEl.classList.add('movie-info');
-      titleEl.textContent = title + ' ';
+      infoEl.classList.add("movie-info");
+      titleEl.textContent = title + " ";
       voteEl.style.backgroundColor = getColor(vote_average);
       voteEl.textContent = vote_average;
-
 
       infoEl.appendChild(titleEl);
       infoEl.appendChild(voteEl);
       movieEl.appendChild(imgEl);
       movieEl.appendChild(infoEl);
       main.appendChild(movieEl);
-
     });
   };
   getMovies(API_URL);
--- search.js
+++ search.js
@@ -1,14 +1,26 @@
 const form = document.getElementById("form");
 const search = document.getElementById("search");
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const searchTerm = search.value;
 
-  console.log("Form submitted");
-  console.log("Search term:", searchTerm);
+  console.log("inviato");
+  console.log("Search", searchTerm);
 
   if (searchTerm) {
-    getMovies(SEARCH_API_URL + "&query=" + searchTerm);
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer 6408e0e2d7398dc7d8998cb43aed056e'
+      }
+    };
+
+    fetch(`https://api.themoviedb.org/3/search/tv?include_adult=true&language=en-US&page=1&query=${searchTerm}`, options)
+      .then(response => response.json())
+      .then(response => console.log(response))
+      .catch(err => console.error(err));
   }
 });
